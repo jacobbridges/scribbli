4ec5895a8ecae4978f57e897da303f98170c80a1
Jacob Bridges <him@jacobandkate143.com>
2017-03-11 14:07:42 -0600

    added tab bar to worlds page and a page model to track tabs

--- alpha/assets/scripts/views/world.ts
+++ alpha/assets/scripts/views/world.ts
@@ -7,6 +7,41 @@ import { worldModel } from '../models/world';
 import { breadcrumbWithoutContainer } from '../components';
 
 
+
+
+const tabs = {
+
+  overview: true,
+  destinations: false,
+  stories: false,
+  characters: false,
+
+  showOverview: function(e: Event) {
+    e.preventDefault();
+    tabs.overview = true;
+    tabs.destinations = tabs.stories = tabs.characters = false;
+  },
+
+  showDestinations: function(e: Event) {
+    e.preventDefault();
+    tabs.destinations = true;
+    tabs.overview = tabs.stories = tabs.characters = false;
+  },
+
+  showStories: function(e: Event) {
+    e.preventDefault();
+    tabs.stories = true;
+    tabs.overview = tabs.destinations = tabs.characters = false;
+  },
+
+  showCharacters: function(e: Event) {
+    e.preventDefault();
+    tabs.characters = true;
+    tabs.overview = tabs.destinations = tabs.stories = false;
+  },
+
+};
+
 export const worldView = checkAuth({
 
   oninit: function(vnode: Mithril.Vnode<any, any>) {
@@ -53,11 +88,61 @@ export const worldView = checkAuth({
   view: function() {
 
     return m('div', [
-      m('.container-fluid.heading-wide', {
-        style: { background: `url(${worldModel.current.background_path}) no-repeat center center` }
-      }, [
+      m('.container-fluid.heading-wide', { style: { background: `url(${worldModel.current.background_path}) no-repeat center center` } }, [
         m(breadcrumbWithoutContainer),
         m('h1', worldModel.current.name),
+      ]),
+      m('.container-fluid.wide-nav', [
+        m('ul.nav.nav-pills.justify-content-center', [
+          m('li.nav-item', [
+            m('a.nav-link [href="#"]', {
+              onclick: tabs.showOverview,
+              'class': tabs.overview ? 'active' : '',
+            }, 'Overview'),
+          ]),
+          m('li.nav-item', [
+            m('a.nav-link [href="#"]', {
+              onclick: tabs.showDestinations,
+              'class': tabs.destinations ? 'active': '',
+            }, 'Destinations'),
+          ]),
+          m('li.nav-item', [
+            m('a.nav-link [href="#"]', {
+              onclick: tabs.showStories,
+              'class': tabs.stories ? 'active': '',
+            }, 'Stories'),
+          ]),
+          m('li.nav-item', [
+            m('a.nav-link [href="#"]', {
+              onclick: tabs.showCharacters,
+              'class': tabs.characters ? 'active': '',
+            }, 'Characters'),
+          ]),
+        ]),
+      ]),
+      // Overview
+      m('.container', { style: { display: tabs.overview ? 'block' : 'none' } }, [
+        m('.row', [
+          m('div', 'Overview'),
+        ]),
+      ]),
+      // Destinations
+      m('.container', { style: { display: tabs.destinations ? 'block' : 'none' } }, [
+        m('.row', [
+          m('div', 'Destinations'),
+        ]),
+      ]),
+      // Stories
+      m('.container', { style: { display: tabs.stories ? 'block' : 'none' } }, [
+        m('.row', [
+          m('div', 'Stories'),
+        ]),
+      ]),
+      // Characters
+      m('.container', { style: { display: tabs.characters ? 'block' : 'none' } }, [
+        m('.row', [
+          m('div', 'Characters'),
+        ]),
       ]),
     ]);
 
