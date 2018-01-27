c4a6a4293ac4b86c79710832b575ee530ec46776
Jacob Bridges <him@jacobandkate143.com>
2018-01-27 12:16:18 -0600

    fab now animates the menu options on hover

--- alpha/assets/scripts/components/fab.ts
+++ alpha/assets/scripts/components/fab.ts
@@ -16,11 +16,70 @@ export interface IFAB {
   mainIcon: string;
 }
 
+interface FabDOMStringMap extends DOMStringMap {
+  name: string;
+  name_id: string;
+}
+interface FabHTMLElement extends HTMLElement{
+  dataset: FabDOMStringMap;
+}
+
 
 // -------------------------------------------------------------------------------------------------
 // Component
 
 export const FAB = {
+
+  oncreate: () => {
+
+    let newIds: string[] = [];
+    Array.prototype.forEach.call(
+      document.getElementsByClassName(
+        'fab-menu-option'), (el: FabHTMLElement) => {
+        el.addEventListener('mouseenter', () => {
+          let nel = document.createElement('span');
+          nel.style.position = 'fixed';
+          nel.style.right = '64px';
+          nel.style.bottom = el.style.bottom;
+          nel.style.lineHeight = '40px';
+          nel.appendChild(document.createTextNode(el.dataset.name));
+          nel.id = 'fab-menu-text-' + (newIds.length + 1);
+          newIds.push(nel.id);
+          el.dataset.name_id = nel.id;
+          document.body.appendChild(nel);
+        });
+        el.addEventListener('mouseleave', () => {
+          document.getElementById(el.dataset.name_id).remove();
+        });
+    });
+    const fab = document.getElementsByClassName('fab-container');
+    fab.item(0).addEventListener('mouseenter', () => {
+      let count = 1;
+      Array.prototype.forEach.call(
+        document.getElementsByClassName(
+          'fab-menu-option'), (el: HTMLElement) => {
+          el.style.bottom = (count*44) + 20 + 'px';
+          el.style.opacity = '1.0';
+          count++;
+      });
+      let fabMenuEl = document
+        .getElementsByClassName('fab-menu')
+        .item(0) as HTMLElement;
+      fabMenuEl.style.height = (count * 44) + 20 + 'px';
+    }, false);
+    fab.item(0).addEventListener('mouseleave', () => {
+      Array.prototype.forEach.call(document.getElementsByClassName('fab-menu-option'), (el: HTMLElement) => {
+        el.style.bottom = '16px';
+        el.style.opacity = '0';
+      });
+      let fabMenuEl = document
+        .getElementsByClassName('fab-menu')
+        .item(0) as HTMLElement;
+      fabMenuEl.style.height = '0';
+      newIds = [];
+    }, false);
+
+  },
 
   view: (vnode: Mithril.Vnode<any, any>) => {
 
